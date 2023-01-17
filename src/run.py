fb06c6b6d3fcd3f53757b61444f976aaf90e5cf2
matthias <m_rosynski@gmx.net>
2023-01-17 18:49:34 +0200

    added visualisation

--- src/run.py
+++ src/run.py
@@ -9,8 +9,10 @@ from pcl_policy.pcl_rainbow.memory import ReplayMemory
 from pcl_policy.pcl_rainbow.rainbow import PCL_rainbow
 from torch.utils.tensorboard import SummaryWriter
 from pcl_policy.greedy.greedy import Greedy
-
-
+import torch
+import plotly
+from plotly.graph_objs import Scatter
+from plotly.graph_objs.scatter import Line
 
 
 def init(args,env, agent,config):
@@ -65,7 +67,7 @@ def init(args,env, agent,config):
                     done=False
                     timeout= False
                 
-                action = agent.epsilon_greedy(T,200000, state)
+                action = agent.epsilon_greedy(T,200, state)
                 #print(action)
 
 
@@ -100,3 +102,78 @@ def init(args,env, agent,config):
 
                 state = next_state
 
+def eval(args, env, agent, config):
+    List2_columns=1
+    List1_row=50
+    s = [[ 0 for x in range(List2_columns)] for i in range (List1_row)]
+    p = [[ 0 for x in range(List2_columns)] for i in range (List1_row)]
+    metrics = {'steps': s, 'points': p}
+    if type(agent) == Greedy:
+        #writer = SummaryWriter()
+        for j in range(List1_row):
+            timeout=False
+            episode=0
+            T, t, done = 0, 0, False
+            sum_reward=0
+            state, _ = env.reset()
+            step = 0
+            done = False
+            print(j)
+            while not done:
+                action = agent.get_action(state)
+                
+                next_state, reward, actions, i, done = env.step(action)
+                sum_reward=sum_reward+reward
+                if step<=65:
+                    metrics['steps'][j].append(step)
+                    metrics['points'][j].append(sum_reward)
+                    step+=1
+                else:
+                    done = True      
+    if type(agent)== PCL_rainbow:
+        for j in range(List1_row):
+            timeout=False
+            episode=0
+            agent.eval()
+            T, t, done = 0, 0, False
+            sum_reward=0
+            state, _ = env.reset()
+            step = 0
+            done = False
+            print(j)
+            while not done:
+                action = agent.make_action(state)
+                next_state, reward, actions, i, done_ = env.step(action)  # Step
+                sum_reward=sum_reward+reward
+                state=next_state
+                if step<=65:
+                    metrics['steps'][j].append(step)
+                    metrics['points'][j].append(sum_reward)
+                    step+=1
+                else:
+                    done = True
+    _plot_line(metrics['steps'], metrics['points'], 'points')
+
+
+
+def _plot_line( xs, ys_population, title, path='/home/matthias/'):
+    max_colour, mean_colour, std_colour, transparent = 'rgb(0, 132, 180)', 'rgb(0, 172, 237)', 'rgba(29, 202, 255, 0.2)', 'rgba(0, 0, 0, 0)'
+    ys = torch.tensor(ys_population, dtype=torch.float32)
+
+    ys_ = ys[0].squeeze()
+
+    ys_min, ys_max, ys_mean, ys_std = np.amax(ys_population, axis=0), np.amin(ys_population, axis=0), ys.mean(0).squeeze(), ys.std(0).squeeze()
+    ys_upper, ys_lower = ys_mean + ys_std, ys_mean - ys_std
+
+
+    
+    trace_max = Scatter(x=xs, y=ys_max, fillcolor=std_colour,  line=Line(color=max_colour, dash='dash'), name='Max')
+    trace_upper = Scatter(x=xs, y=ys_upper.numpy(), fillcolor=std_colour, line=Line(color=transparent), name='+1 Std. Dev.', showlegend=False) #line=Line(color=transparent), name='+1 Std. Dev.', showlegend=False)
+    trace_mean = Scatter(x=xs, y=ys_mean,  fill='tonexty',  fillcolor=std_colour, line=Line(color=mean_colour), name='Mean')
+    trace_lower = Scatter(x=xs, y=ys_lower.numpy(),fill='tonexty',  fillcolor=std_colour, line=Line(color=transparent), name='-1 Std. Dev.', showlegend=False)
+    trace_min = Scatter(x=xs, y=ys_min, line=Line(color=max_colour, dash='dash'), name='Min')
+
+    plotly.offline.plot({
+        'data': [trace_max,trace_upper, trace_mean, trace_lower, trace_min],
+        'layout': dict(font_size=40,title=title, xaxis={'title': 'Step'}, yaxis={'title': title}),
+    }, filename=os.path.join(path, title + '.html'), auto_open=False)
